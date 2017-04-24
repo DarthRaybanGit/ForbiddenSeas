4ffce506e6f962d6f4a3ab17e96573696ffd988b
Matteo Marras <matteo.marras1994@gmail.com>
2017-04-24 17:07:34 +0200

    Game Dev
    	- Adjusted Pirates Flagship Prefab
    	- Fixed Network Start Scene

--- ForbiddenSeas/Assets/Scripts/MoveSimple.cs
+++ ForbiddenSeas/Assets/Scripts/MoveSimple.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class MoveSimple : MonoBehaviour {
 
     public float speed, rotSpeed;
-	
+
 	void Update ()
     {
         Vector3 move = Camera.main.transform.forward * Input.GetAxis("Vertical") * Time.deltaTime * speed;
         transform.position += new Vector3(move.x, 0f, move.z);
-        transform.Rotate(new Vector3(0f, 0f, Input.GetAxis("Horizontal"))* Time.deltaTime * rotSpeed); 
+        transform.Rotate(new Vector3(0f, Input.GetAxis("Horizontal") * Time.deltaTime * rotSpeed, 0f ));
 	}
 }
