0bfad1e73ff0513419d9c37726301d1910d3205d
Matteo Marras <matteo.marras1994@gmail.com>
2017-04-24 17:45:03 +0200

    Game Dev
    	- Added Symbols script in order to collect Enums and Constants
    	- Started building Pirates Flagship class

---
+++ ForbiddenSeas/Assets/Scripts/PiratesFlagship.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PiratesFlagship : MonoBehaviour {
+
+
+    public static int m_MaxHealth = 1000;
+    public int m_Health = m_MaxHealth;
+
+    public float m_Maneuvrability = ManeuverabilityLevel.LOW;
+    public float m_Speed = 60f;
+
+
+    // Use this for initialization
+    void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ ForbiddenSeas/Assets/Scripts/Symbols.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class SpeedLevel
+{
+    public const float STOP = 0f;
+    public const float LOW = 0.25f;
+    public const float HALF = 0.5f;
+    public const float HIGH = 1f;
+}
+
+public class ManeuverabilityLevel
+{
+    public const float LOW = 0.3f;
+    public const float HALF = 0.6f;
+    public const float HIGH = 1f;
+}
+
+public class SpeedLevelRatio
+{
+    public const float LOW = 1f;
+    public const float HALF = 1.5f;
+    public const float HIGH = 2f;
+}
