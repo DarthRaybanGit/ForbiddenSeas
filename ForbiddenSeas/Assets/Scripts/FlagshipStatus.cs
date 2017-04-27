80d8e3dbdcb7b6d1fa87dfdf2d8c9a698a3c09cc
Manuel Salvadori <morke.bm@hotmail.it>
2017-04-27 17:35:45 +0200

    Aggiornato script Status Flagship

---
+++ ForbiddenSeas/Assets/Scripts/FlagshipStatus.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class FlagshipStatus : NetworkBehaviour
+{
+    public ShipClass shipClass;
+    public static string shipName;
+    public static int m_MaxHealth;
+    [SyncVar]
+    public int m_Health;
+    public static float m_Maneuvrability;
+    public static float m_maxSpeed;
+
+    void Start()
+    {
+        switch (shipClass)
+        {
+            case ShipClass.pirates:
+                shipName = Pirates.shipName;
+                m_MaxHealth = Pirates.maxHealth;
+                m_Maneuvrability = Pirates.maneuverability;
+                m_maxSpeed = Pirates.maxSpeed;
+                break;
+
+            case ShipClass.vikings:
+                shipName = Vikings.shipName;
+                m_MaxHealth = Vikings.maxHealth;
+                m_Maneuvrability = Vikings.maneuverability;
+                m_maxSpeed = Vikings.maxSpeed;
+                break;
+
+            case ShipClass.venetians:
+                shipName = Venetians.shipName;
+                m_MaxHealth = Venetians.maxHealth;
+                m_Maneuvrability = Venetians.maneuverability;
+                m_maxSpeed = Venetians.maxSpeed;
+                break;
+
+            case ShipClass.orientals:
+                shipName = Orientals.shipName;
+                m_MaxHealth = Orientals.maxHealth;
+                m_Maneuvrability = Orientals.maneuverability;
+                m_maxSpeed = Orientals.maxSpeed;
+                break;
+
+            default:
+                return;
+
+        }
+        m_Health = m_MaxHealth;
+    }
+    public enum ShipClass {pirates, vikings, venetians, orientals};
+
+}
+
--- ForbiddenSeas/Assets/Scripts/MoveSimple.cs
+++ ForbiddenSeas/Assets/Scripts/MoveSimple.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class MoveSimple : MonoBehaviour {
+public class MoveSimple : NetworkBehaviour {
 
-    public float speed, rotSpeed;
+    public float speed;
+    public float rotSpeed;
 
 	void Update ()
     {
+        if (!isLocalPlayer)
+            return;
+        
         Vector3 move = Camera.main.transform.forward * Input.GetAxis("Vertical") * Time.deltaTime * speed;
         transform.position += new Vector3(move.x, 0f, move.z);
         transform.Rotate(new Vector3(0f, Input.GetAxis("Horizontal") * Time.deltaTime * rotSpeed, 0f ));
