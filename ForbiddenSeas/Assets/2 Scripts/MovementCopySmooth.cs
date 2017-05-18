2cfa37bef97d3601827710db30c68751ebe6bdb6
Alberto Ronchetti <happyalby@hotmail.it>
2017-05-18 12:55:06 +0200

    ho aggiunto come nell'altro script un valore di rotazione aggiuntivo in gaso di input laterale

--- ForbiddenSeas/Assets/2 Scripts/MovementCopySmooth.cs
+++ ForbiddenSeas/Assets/2 Scripts/MovementCopySmooth.cs
@@ -7,7 +7,8 @@ public class MovementCopySmooth : MonoBehaviour {
 	// Use this for initialization
 	public GameObject player;
 	private float velocity=0f;
-	public float smooth=1f;
+	public float smooth=0.1f;
+	public float Angle = 15f;
 
 	void Start () {
 		transform.position = player.transform.position;
@@ -17,7 +18,7 @@ public class MovementCopySmooth : MonoBehaviour {
 	void FixedUpdate () {
 		transform.position = player.transform.position;
 		Vector3 EulerAngles = transform.rotation.eulerAngles;
-		EulerAngles.y = Mathf.SmoothDampAngle (EulerAngles.y, player.transform.eulerAngles.y, ref velocity, smooth);
+		EulerAngles.y = Mathf.SmoothDampAngle (EulerAngles.y, player.transform.eulerAngles.y + Input.GetAxis("Horizontal") * Angle, ref velocity, smooth);
 		transform.rotation = Quaternion.Euler (EulerAngles);
 	}
 }
