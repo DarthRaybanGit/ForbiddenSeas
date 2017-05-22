04b02cbb42a936905ed2314928063f0849f8bf7c
Alberto Ronchetti <happyalby@hotmail.it>
2017-05-22 16:35:47 +0200

    su e giu, qua e la, questo mondo fa girar(animazioni)

--- ForbiddenSeas/Assets/2 Scripts/Ships/MoveSimple.cs
+++ ForbiddenSeas/Assets/2 Scripts/Ships/MoveSimple.cs
@@ -15,20 +15,10 @@ public class MoveSimple : NetworkBehaviour {
 	private float deceleration = 0.1f;
 	public float Factor=0;
 	private float Scroll = 0;
-	//private float forward=1;
-	//private float Velocity=0f;
 	private float smoothTime=20f;
 	private Rigidbody rb;
 	public float numberOfScroll = 20f;
-
-	/* float waterLevel = 0f;
-	public float floatHeight = 2f;
-	public float bounceDamp = 0.05f;
-	public Vector3 bouyancyCentreOffset;
-
-	private float forceFactor;
-	private Vector3 actionPoint;
-	private Vector3 upLift;*/
+	public Animator animator;
 
 	public override void OnStartLocalPlayer()
 	{
@@ -41,6 +31,7 @@ public class MoveSimple : NetworkBehaviour {
         yield return new WaitForFixedUpdate();
         maxSpeed = GetComponent<FlagshipStatus>().m_maxSpeed;
         maneuvrability = GetComponent<FlagshipStatus>().m_Maneuvrability;
+		animator = GetComponentInChildren<Animator> ();
     }
 
 	void FixedUpdate ()
@@ -72,10 +63,6 @@ public class MoveSimple : NetworkBehaviour {
 					Factor = SpeedLevel.SLOW;
 			}
 		}
-		/*if (actualspeed<speed*factor*forward)
-			actualspeed +=acceleration*time.deltatime;*/
-		//Vector3 moveVelocity = new Vector3 (0, 0, speed * Factor * forward * -1);
-		/*if (rb.velocity.magnitude < maxSpeed * Factor ) */
 		if (ActualSpeed<maxSpeed*State/numberOfScroll)
 			ActualSpeed = Mathf.Lerp (ActualSpeed, maxSpeed * State/numberOfScroll, Acceleration);
 		else
@@ -84,30 +71,8 @@ public class MoveSimple : NetworkBehaviour {
 		var desiredRotation=Quaternion.Euler(new Vector3(0f,transform.rotation.eulerAngles.y + Input.GetAxis("Horizontal") * Time.deltaTime * rotSpeed * maneuvrability , Input.GetAxis ("Horizontal") * 10f * Factor * -1));
 		transform.rotation= Quaternion.Lerp(transform.rotation, desiredRotation, Time.deltaTime * smoothTime);
 		rb.angularVelocity = Vector3.zero;
-		//rb.AddTorque (transform.up * Input.GetAxis ("Horizontal") * rotSpeed);
-		//Debug.Log (rb.velocity.magnitude);
-		//GetClosestVertex ();
-		/*actionPoint = transform.position + transform.TransformDirection(bouyancyCentreOffset);
-		forceFactor = 1f - ((actionPoint.y - waterLevel) / floatHeight);
-		if (forceFactor > 0f)
-		{
-			upLift = -Physics.gravity * (forceFactor - rb.velocity.y * bounceDamp);
-			Debug.Log (upLift);
-			rb.AddForceAtPosition (upLift, actionPoint);
-		}*/
+		animator.SetFloat ("Speed", ActualSpeed / maxSpeed);
+		Debug.Log (State / numberOfScroll);
 	}
-
-	/*void GetClosestVertex()
-	{
-		for (int i = 0; i < planeCloth.vertices.Length; i++) {
-			if (index == -1)
-				index = i;
-			float Distance=Vector3.Distance(planeCloth.vertices[i],transform.position);
-			float closestDistance = Vector3.Distance (planeCloth.vertices [index],transform.position);
-			if (Distance < closestDistance)
-				index = i;
-		}
-		transform.localPosition = new Vector3 (transform.localPosition.x, planeCloth.vertices [index].y, transform.localPosition.z);
-	}*/
 
 }
