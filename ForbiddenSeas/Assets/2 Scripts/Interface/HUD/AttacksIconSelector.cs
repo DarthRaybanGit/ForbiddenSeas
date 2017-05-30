71a21fdf638cfb32cc0a8d6057107c7efe1fb63d
Manuel Salvadori <morke.bm@hotmail.it>
2017-05-30 15:12:16 +0200

    icone attacchi UI added

---
+++ ForbiddenSeas/Assets/2 Scripts/Interface/HUD/AttacksIconSelector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AttacksIconSelector : MonoBehaviour
+{
+    public bool isMain = true;
+    public Sprite[] iconsMain;
+    public Sprite[] iconsSpec;
+
+    void Start()
+    {
+        StartCoroutine(WaitforReady());
+    }
+
+    IEnumerator WaitforReady()
+    {
+        yield return new WaitUntil(() => LocalGameManager.Instance.IsEveryPlayerRegistered());
+
+        if(isMain)
+            GetComponent<Image>().sprite = iconsMain[(int)LocalGameManager.Instance.m_LocalPlayer.GetComponent<FlagshipStatus>().shipClass];
+        else
+            GetComponent<Image>().sprite = iconsSpec[(int)LocalGameManager.Instance.m_LocalPlayer.GetComponent<FlagshipStatus>().shipClass];
+        
+    }
+}
