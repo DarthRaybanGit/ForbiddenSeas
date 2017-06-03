e89e26b71c51a6aad39f9c996080437f01fe1e2d
Matteo Marras <matteo.marras1994@gmail.com>
2017-06-03 20:29:45 +0200

    Messaggio per l'arrhhh

---
+++ ForbiddenSeas/Assets/2 Scripts/GameObjects/GateToCanali.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateToCanali : MonoBehaviour {
+
+	private void OnTriggerEnter(Collider other)
+    {
+        if (LocalGameManager.Instance.isServer)
+        {
+            if (other.gameObject.GetComponent<Player>())
+            {
+                other.gameObject.GetComponent<Player>().m_InsideArena = !other.gameObject.GetComponent<Player>().m_InsideArena;
+            }
+        }
+    }
+}
