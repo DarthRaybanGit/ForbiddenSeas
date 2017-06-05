eae6b26ecac17d3b9e65816320acb7a91239095f
Manuel Salvadori <morke.bm@hotmail.it>
2017-06-05 15:27:22 +0200

    statistic updato gozaimasu

---
+++ ForbiddenSeas/Assets/TiFaccioVedereIo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiFaccioVedereIo : MonoBehaviour
+{
+
+    public GameObject pirates;
+	
+    public void EnableThing()
+    {
+        pirates.SetActive(true);
+    }
+}
