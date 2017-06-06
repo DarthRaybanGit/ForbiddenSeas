164b8a62333c27bd8ee3e6508335f55a9fe10f55
Matteo Marras <matteo.marras1994@gmail.com>
2017-06-06 16:51:33 +0200

    Mina

---
+++ ForbiddenSeas/Assets/2 Scripts/GameObjects/Mina.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Mina : NetworkBehaviour {
+
+    public int m_Danno;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (isServer)
+        {
+            if (collision.gameObject.GetComponent<Player>() && !collision.gameObject.GetComponent<FlagshipStatus>().m_isDead)
+            {
+                GetComponent<MeshRenderer>().enabled = false;
+                transform.GetChild(0).gameObject.SetActive(true);
+                GetComponent<Collider>().enabled = false;
+                collision.gameObject.GetComponent<FlagshipStatus>().PrendiDannoDaEnemy(m_Danno);
+                StartCoroutine(shutDownMe());
+
+            }
+        }
+    }
+
+    IEnumerator shutDownMe()
+    {
+        yield return new WaitForSeconds(2.5f);
+        Destroy(gameObject);
+    }
+}
