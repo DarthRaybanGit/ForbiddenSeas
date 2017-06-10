2bc5d926b8c890f0950da8a18702efc53f587fbd
Manuel Salvadori <morke.bm@hotmail.it>
2017-06-10 16:59:38 +0200

    gitto commicto meno emmu più sb xflcjvbaej<senfbdv c non funziona un a cippa c'è solo da testare

---
+++ ForbiddenSeas/Assets/EndGameStats.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndGameStats : MonoBehaviour
+{
+    public Text [] names;
+    public Text [] arrhs;
+    public Text [] kills;
+    public Text [] deaths;
+    public Text [] reputations;
+    public Image [] classes;
+    public Sprite [] sprites;
+
+    void OnEnable()
+    {
+        ShowStats();
+    }
+
+    void ShowStats()
+    {
+        for (int i = 0; i < LocalGameManager.Instance.m_Players.Length; i++)
+        {
+            names[i].text = LocalGameManager.Instance.GetPlayer(i+1).GetComponent<Player>().playerName;
+            kills[i].text = LocalGameManager.Instance.m_playerKills[i].ToString();
+            arrhs[i].text = LocalGameManager.Instance.m_playerArrh[i].ToString();
+            deaths[i].text = LocalGameManager.Instance.m_playerDeaths[i].ToString();
+            reputations[i].text = LocalGameManager.Instance.GetPlayer(i + 1).GetComponent<FlagshipStatus>().m_reputation.ToString();
+            classes[i].sprite = sprites[(int)LocalGameManager.Instance.GetPlayer(i+1).GetComponent<FlagshipStatus>().shipClass];
+        }
+    }
+
+    public void returnToLobby()
+    {
+        //nfewjin nj doicarjtu pa, no dearsweoy myojirtu arrh!
+    }
+}
