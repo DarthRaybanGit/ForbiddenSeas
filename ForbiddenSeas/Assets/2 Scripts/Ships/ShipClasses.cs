5060edf5dc9065b0f54448ac0a2fef95b9064c4c
Alberto Ronchetti <happyalby@hotmail.it>
2017-06-21 16:55:00 +0200

    fix danni

--- ForbiddenSeas/Assets/2 Scripts/Ships/ShipClasses.cs
+++ ForbiddenSeas/Assets/2 Scripts/Ships/ShipClasses.cs
@@ -5,12 +5,12 @@ public class Pirates
 {
     public const string shipName = "Pirate Galleon";
     public const int maxHealth = 1000;
-    public const float maxSpeed = 12f;
+    public const float maxSpeed = 10f;
     public const float maneuverability = ManeuverabilityLevel.LOW;
     public const int mainAttackDmg = 450;
     public const float mainAttackCD = 2.5f;
     public const int specAttackDmg = 500;
-    public const float specAttackCD = 10f;
+    public const float specAttackCD = 5f;
     public const float defense = 0.15f;
     public const float mainDistance = -20f;
     public const float specialDistance = 5f;
@@ -22,9 +22,9 @@ public class Egyptians
 {
     public const string shipName = "Solar Ship";
     public const int maxHealth = 800;
-    public const float maxSpeed = 10f;
+    public const float maxSpeed = 12f;
     public const float maneuverability = ManeuverabilityLevel.HIGH;
-    public const int mainAttackDmg = 300;
+    public const int mainAttackDmg = 400;  //FIXME
     public const float mainAttackCD = 2.2f;
     public const int specAttackDmg = 500;
     public const float specAttackCD = 10f;
@@ -44,7 +44,7 @@ public class Vikings
     public const int mainAttackDmg = 300;
     public const float mainAttackCD = 2.2f;
     public const int specAttackDmg = 500;
-    public const float specAttackCD = 10f;
+    public const float specAttackCD = 5f;
     public const float defense = 0.10f;
     public const float mainDistance = -22f;
     public const float specialDistance = 5f;
