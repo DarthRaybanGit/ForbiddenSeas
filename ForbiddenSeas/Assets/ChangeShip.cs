c28bbf6db91bb5ec300f422aabf697b6af886ce9
Manuel Salvadori <morke.bm@hotmail.it>
2017-06-21 14:42:24 +0200

    fix blind

---
+++ ForbiddenSeas/Assets/ChangeShip.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeShip : MonoBehaviour
+{
+
+    public OutGameCanvasControl ogcc;
+
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            ogcc.prevClassSelect();   
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            ogcc.nextClassSelect();
+        }
+	}
+}
