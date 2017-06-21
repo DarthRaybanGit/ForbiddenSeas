6035cdf250e7c971f233abbe8bb250944fc7c0dd
Manuel Salvadori <morke.bm@hotmail.it>
2017-06-21 09:32:53 +0200

    fix blind

--- ForbiddenSeas/Assets/2 Scripts/Canvas/Blind.cs
+++ ForbiddenSeas/Assets/2 Scripts/Canvas/Blind.cs
@@ -10,6 +10,7 @@ public class Blind : MonoBehaviour
     public void SetBlind(bool b)
     {
         GetComponent<Image>().enabled = b;
+        GetComponent<Image>().color = b ? new Color(GetComponent<Image>().color.r, GetComponent<Image>().color.g, GetComponent<Image>().color.b, 214f / 255f) : new Color(GetComponent<Image>().color.r, GetComponent<Image>().color.g, GetComponent<Image>().color.b, 0f);
         Camera.main.GetComponent<VignetteAndChromaticAberration>().enabled = b;
     }
 }
