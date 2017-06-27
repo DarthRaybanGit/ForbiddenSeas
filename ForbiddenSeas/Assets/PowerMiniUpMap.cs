6f8227a4ff383204084940dcae977eba0fd97bfb
Manuel Salvadori <morke.bm@hotmail.it>
2017-06-27 14:36:30 +0200

    gitto pushitto

---
+++ ForbiddenSeas/Assets/PowerMiniUpMap.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerMiniUpMap : MonoBehaviour
+{
+    [Range(1,3)]
+    public int p_up;
+    private bool ready = false;
+    private Vector3 normalizedPowerPos;
+
+    void Start()
+    {
+        StartCoroutine(WaitforReady());
+    }
+
+    IEnumerator WaitforReady()
+    {
+        yield return new WaitUntil(() => LocalGameManager.Instance.IsEveryPlayerRegistered());
+        yield return new WaitForSeconds(0.2f);
+        SetPos();
+    }
+        
+    void SetPos ()
+    {
+        normalizedPowerPos = (OnlineManager.s_Singleton.m_PowerUpSpawnPosition[p_up].transform.position) / 427.5f * 150f;
+        GetComponent<RectTransform>().anchoredPosition = new Vector2(normalizedPowerPos.x, normalizedPowerPos.z);
+    }
+}
